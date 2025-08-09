37b68e86ea7f7571fae165c8c98d1488eb9d26a9
Starry <jonb62031@gmail.com>
2025-08-09 21:41:07 +0300

    add new function and commands

--- cogs/user_cmd.py
+++ cogs/user_cmd.py
@@ -2,7 +2,8 @@ import disnake
 
 from disnake.ext import commands
 from disnake import TextInputStyle
-from database.database import edit_user_info
+from database.database import edit_user_info, get_user_info, user_get_tasks
+from cogs.registration import RegistrationWindow
 
 # -- user info --
 # intents = disnake.Intents.default()
@@ -11,88 +12,42 @@ from database.database import edit_user_info
 
 
 class CmdUsers(commands.Cog):
-
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    # реєстрація
+    @commands.slash_command(name="regist", description="Почати реєстрацію")
+    async def registration(self, inter: disnake.ApplicationCommandInteraction):
+        # тимчасове поки не з'явитьсяя перевірка на доступ до команд
+        # ------------------------------------------------------------
+        user_data = await get_user_info(inter.author.id)
+        if user_data is not None:
+            await inter.response.send_message("Ви вже зареєстровані", ephemeral=True)
+            return
+        # ------------------------------------------------------------
+        await inter.response.send_modal(RegistrationWindow())
 
-    # # інформація про користувача
-    # @commands.slash_command(name='editinfo', description='редагує інформацію про користувача')
-    # async def edit_info(self, inter: disnake.ApplicationCommandInteraction):
-    #     await inter.response.send_message("Що ви хочете змінити?",
-    #         components=[
-    #             disnake.ui.Button(label="Ім'я", style=disnake.ButtonStyle.grey, custom_id="name"),
-    #             disnake.ui.Button(label="карта", style=disnake.ButtonStyle.grey, custom_id="card"),
-    #             disnake.ui.Button(label="роль", style=disnake.ButtonStyle.grey, custom_id="role"),
-    #             disnake.ui.Button(label="все", style=disnake.ButtonStyle.grey, custom_id="all"),
-    #         ],
-    #     )
+    # редагування інформації
+    @commands.slash_command(name="editinfo", description="редагування даних")
+    async def editinfo(self, inter: disnake.ApplicationCommandInteraction):
+        user_data = await get_user_info(inter.author.id)
+        if user_data is None:
+            await inter.response.send_message("Ви не зареєстровані! Спочатку зареєструйтесь ", ephemeral=True)
+            return
+        else: 
+            await inter.response.send_modal(RegistrationWindow(is_edit=True, current_data=user_data))
     
-#     @commands.Cog.listener("on_button_click")
-#     async def change_info(self, inter: disnake.MessageInteraction):
-#         if inter.component.custom_id not in ["name", "card", "role", "all"]:
-#             return
-#         if inter.component.custom_id == "name":
-#             await inter.response.send_message("Змінення імені користувача")
-#         elif inter.component.custom_id == "card":
-#             await inter.response.send_message("Змінення карти користувача")
-#         elif inter.component.custom_id == "role":
-#             await inter.response.send_message("Змінення ролі користувача")
-#         elif inter.component.custom_id == "all":
-#             await inter.response.send_message("Змінення всіх даних користувача")
 
-
-# class EditWindow(disnake.ui.Modal):
-#     def __init__(self, value):
-#         if value == "name":
-#             components = [
-#                 disnake.ui.TextInput(
-#                     label="ім'я",
-#                     placeholder="Введіть своє ім'я",
-#                     custom_id="username",
-#                     style=TextInputStyle.short,
-#                     min_length=2,
-#                     max_length=50,
-#                     required=True,
-#                 ),
-#             ],
-#         elif value == "card":
-#             components = [
-#                 disnake.ui.TextInput(
-#                     label="карта",
-#                     placeholder="Введіть номер своєї карти",
-#                     custom_id="card",
-#                     style=TextInputStyle.short,
-#                     min_length=2,
-#                     max_length=50,
-#                     required=True,
-#                 ),
-#             ],
-#         elif value == "all":
-#             components = [
-#                 disnake.ui.TextInput(
-#                     label="Змінити все",
-#                     placeholder="Введіть своє ім'я",
-#                     custom_id="username",
-#                     style=TextInputStyle.short,
-#                     min_length=2,
-#                     max_length=50,
-#                     required=True,
-#                 ),
-#                 disnake.ui.TextInput(
-#                     label="карта",
-#                     placeholder="Введіть номер своєї карти",
-#                     custom_id="bank_card",
-#                     style=TextInputStyle.short,
-#                     min_length=16,
-#                     max_length=20,
-#                     required=True,
-#                 ),
-#             ]
-#             super().__init__(title="Реєстрація", components=components)
-
-
-
+    @commands.slash_command(name="mytasks", description="перегляд взятих завдань")
+    async def mytasks(self, inter: disnake.ApplicationCommandInteraction):
+        user_id = inter.author.id 
+        tasks_info = await user_get_tasks(user_id)
+        print(tasks_info)
+        if tasks_info is None:
+            await inter.response.send_message("Ви  ще не маєте завдань")
+            return
+        else:
+            await inter.response.send_message("Ваші завдання: ")
 
 
 def setup(bot: commands.Bot):
