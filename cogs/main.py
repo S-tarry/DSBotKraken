02b4183d5cb3600f434abb3780532ea3f19e1546
Starry <jonb62031@gmail.com>
2025-09-06 17:57:35 +0300

    add new functions and bug fix

--- cogs/main.py
+++ cogs/main.py
@@ -1,5 +1,7 @@
 import disnake
 from disnake.ext import commands, tasks
+from database.requests import add_all_roles_into_db
+from config.config import ADMIN_ID, SERVER_ID, NOT_REGIST_ID, START_CHANNEL
 
 
 intents = disnake.Intents.default()
@@ -13,18 +15,31 @@ class Main(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    
-    
-    
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        guild = self.bot.get_guild(SERVER_ID)
+        if guild:
+            await add_all_roles_into_db(guild.roles, [ADMIN_ID])
+            print("All roles add into db")
 
 
     @commands.Cog.listener()
     async def on_member_join(self, member: disnake.Member):
-        channel = disnake.utils.get(member.guild.text_channels, name="чат")
+        role = member.guild.get_role(NOT_REGIST_ID)
+        if role:
+            await member.add_roles(role)
+        channel = self.bot.get_channel(START_CHANNEL)
         if channel:
-            await channel.send("Привіт, я бот Kraken Gamers призначений для покращення комунікації... Спочатку вам потрібно пройти реєстрацію для цього використовуйте команду /register")
+            await channel.send("Привіт, я бот Kraken Gamers призначений для покращення комунікації... Спочатку вам потрібно пройти реєстрацію для цього використовуйте команду /regist")
 
     
+    @commands.Cog.listener()
+    async def on_guild_role_create(self, role: disnake.Role):
+        await add_all_roles_into_db([role], [ADMIN_ID])
+        print(f"New role - {role.name} add to db")
+
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Main(bot))
